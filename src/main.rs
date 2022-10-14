df62b5a3f7d932c4bc0884d93d6a866f04bbe777
WukoSiric <lstainer2002@gmail.com>
2022-10-15 00:46:05 +1030

    Driver function works, no win-state

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ const WORD_LIST: [&str; 50] = [
 
 fn choose_word(word_list: [&str; 50]) -> String {
     let rng = rand::thread_rng().gen_range(0..WORD_LIST.len());
-    return word_list[rng].to_string()
+    return word_list[rng].to_string().to_uppercase();
 }
 
 use std::io;
@@ -26,10 +26,13 @@ fn get_input() -> String {
         guess = guess.trim().to_string();
         guess_length = guess.len();
     }
+
+    let guess = guess.to_uppercase();
     return guess
 }
 
 fn guess_letter(word: &str, letter: String) -> bool {
+    let letter = letter.to_uppercase();
     return word.contains(letter.as_str())
 }
 
@@ -57,7 +60,35 @@ fn print_word(word: &str, correct_letters: &HashSet<String>) -> String {
 }
 
 fn main() {
-    println!("Hello, world!");
+    print!("\x1B[2J\x1B[1;1H"); //Clears the screen
+    let chosen_word = choose_word(WORD_LIST);
+    let mut guesses = 0;
+    const MAX_GUESSES: u32 = 6;
+    let mut correct_letters: HashSet<String> = HashSet::new();
+
+    println!("Welcome to Hangman!");
+    println!("A word has been chosen, it is {} letters long", chosen_word.len());
+
+    while guesses < MAX_GUESSES {
+
+        println!("Guess a letter");
+        let guess = get_input();
+        let letter_exists = update_correct_letters(&chosen_word, guess, &mut correct_letters);
+        print!("\x1B[2J\x1B[1;1H"); //Clears the screen
+        if letter_exists {
+            println!("Correct!");
+        } else {
+            println!("Incorrect!");
+            guesses += 1;
+        }
+        println!("You have {} guesses left", MAX_GUESSES - guesses);
+        println!("{}", print_word(&chosen_word, &correct_letters));
+
+    }
+
+    println!("You lose!");
+    println!("The word was: {}", chosen_word);
+
 }
 
 #[test]
@@ -68,45 +99,44 @@ fn can_choose_word() {
 
 #[test]
 fn can_guess_letter_correct() {
-    let word = "apple";
-    let guess = String::from("a"); 
+    let word = "APPLE";
+    let guess = String::from("A"); 
     assert_eq!(true, guess_letter(word, guess));
 }
 
 #[test]
 fn can_guess_letter_incorrect() {
-    let word = "apple";
-    let guess = String::from("b"); 
+    let word = "APPLE";
+    let guess = String::from("B"); 
     assert_eq!(false, guess_letter(word, guess));
 }
 
-#[test]
-fn can_input() {
-    let input = get_input();
-    println!("a");
-    assert_eq!(1, input.len());
-}
+// #[test]
+// fn can_input() {
+//     let input = get_input();
+//     assert_eq!(1, input.len());
+// }
 
 #[test]
 fn can_update_letters_correct() {
     let mut correct_letters: HashSet<String> = HashSet::new();
-    let chosen_word = String::from("Hippo");
-    let guess = String::from("p");
+    let chosen_word = String::from("HIPPO");
+    let guess = String::from("P");
     assert_eq!(update_correct_letters(chosen_word.as_str(), guess, &mut correct_letters), true);
 }
 
 #[test]
 fn can_update_letters_incorrect() {
     let mut correct_letters: HashSet<String> = HashSet::new();
-    let chosen_word = String::from("Hippo");
-    let guess = String::from("a");
+    let chosen_word = String::from("HIPPO");
+    let guess = String::from("A");
     assert_eq!(update_correct_letters(chosen_word.as_str(), guess, &mut correct_letters), false);
 }
 
 #[test]
 fn can_print_word() {
     let mut correct_letters: HashSet<String> = HashSet::new();
-    let chosen_word = String::from("Hippo");
+    let chosen_word = String::from("HIPPO");
     let guess = String::from("H");
     update_correct_letters(chosen_word.as_str(), guess, &mut correct_letters);
     assert_eq!(print_word(chosen_word.as_str(), &correct_letters), "H _ _ _ _ ");
